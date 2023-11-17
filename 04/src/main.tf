e1dd84f35cbe33a79ba900c6df34185a452d1f35
Ivan Malyshev <ivanmalyshev1994@gmail.com>
2023-11-17 23:06:03 +0300

    hw-04

--- 04/src/main.tf
+++ 04/src/main.tf
@@ -7,8 +7,8 @@ module "vpc" {
   vpc_folder_id = var.folder_id
   subnets      = [
     { zone = "ru-central1-a", v4_cidr_blocks = "10.0.1.0/24" },
-    { zone = "ru-central1-b", v4_cidr_blocks = "10.0.2.0/24" },
-    { zone = "ru-central1-c", v4_cidr_blocks = "10.0.3.0/24" }
+    #{ zone = "ru-central1-b", v4_cidr_blocks = "10.0.2.0/24" },
+    #{ zone = "ru-central1-c", v4_cidr_blocks = "10.0.3.0/24" }
   ]
 }
 
